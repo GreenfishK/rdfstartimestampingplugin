a5170b94bd228c6b99d17e4f4cbe521f15be3b11
Filip Kovacevic <f.kovacevic@gmx.at>
2022-03-10 12:07:43 +0100

    <fix>: change isIRI, isBNode, isLiteral to instanceof SimpleIRI, SimpleLiteral, SimpleBnode. There is a problem with these boolean methods because it raises the NoSuchMethodException.

--- src/main/java/com/ontotext/trree/plugin/rdfstartimestamping/RDFStarTimestampingPlugin.java
+++ src/main/java/com/ontotext/trree/plugin/rdfstartimestamping/RDFStarTimestampingPlugin.java
@@ -3,20 +3,22 @@ package com.ontotext.trree.plugin.rdfstartimestamping;
 import com.ontotext.trree.sdk.*;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.impl.SimpleBNode;
+import org.eclipse.rdf4j.model.impl.SimpleIRI;
+import org.eclipse.rdf4j.model.impl.SimpleLiteral;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
 
+import java.util.Arrays;
+
 public class RDFStarTimestampingPlugin extends PluginBase implements StatementListener, PluginTransactionListener{
 
 	private static final String PREFIX = "http://example.com/";
-
-	private static final String TIME_PREDICATE = PREFIX + "time";
-	private static final String GO_FUTURE_PREDICATE = PREFIX + "goInFuture";
-	private static final String GO_PAST_PREDICATE = PREFIX + "goInPast";
-
-	private final Repository repo = new SPARQLRepository("http://localhost:7200/repositories/test_timestamping/statements");
+	private String getEndpoint;
+	private String postEndpoint;
+	private Repository repo;
 	private String updateString;
 	private boolean triplesTimestamped = false;
 
@@ -32,24 +34,26 @@ public class RDFStarTimestampingPlugin extends PluginBase implements StatementLi
 	public void initialize(InitReason reason, PluginConnection pluginConnection) {
 		// Create IRIs to represent the entities
 		getLogger().info("rdf-star-timestamping plugin initialized!");
+		this.getEndpoint = "http://localhost:7200/repositories/test_timestamping";
+		this.postEndpoint = "http://localhost:7200/repositories/test_timestamping/statements";
 	}
 
 	@Override
 	public boolean statementAdded(long subject, long predicate, long object, long context, boolean isAddition, PluginConnection pluginConnection) {
-		String s = pluginConnection.getEntities().get(subject).stringValue();
-		String o = pluginConnection.getEntities().get(predicate).stringValue();
-		String p = pluginConnection.getEntities().get(object).stringValue();
+		Value s = pluginConnection.getEntities().get(subject);
+		Value o = pluginConnection.getEntities().get(predicate);
+		Value p = pluginConnection.getEntities().get(object);
 		getLogger().info("Statement added:" + s + " " + p + " " + o + " within context:" + context);
 
-		/*if (!triplesTimestamped) {
+		if (!triplesTimestamped) {
 			updateString = String.format("insert { << %s %s %s >> " +
 							"<http://example.com/metadata/versioning#valid_from> ?timestamp } " +
 							"where {BIND(<http://www.w3.org/2001/XMLSchema#dateTime>(NOW()) AS ?timestamp) }",
-					entityToString(pluginConnection.getEntities().get(subject)),
-					entityToString(pluginConnection.getEntities().get(predicate)),
-					entityToString(pluginConnection.getEntities().get(object)));
+					entityToString(s),
+					entityToString(o),
+					entityToString(p));
 			getLogger().info(updateString);
-		}*/
+		}
 
 		return false;
 	}
@@ -64,11 +68,11 @@ public class RDFStarTimestampingPlugin extends PluginBase implements StatementLi
 	}
 
 	private String entityToString(Value value) {
-		if (value.isIRI())
+		if (value instanceof SimpleIRI)
 			return "<" + value + ">";
-		if (value.isLiteral())
+		if (value instanceof SimpleLiteral)
 			return value.toString();
-		if (value.isBNode())
+		if (value instanceof SimpleBNode)
 			return value.toString();
 		getLogger().error("The entity's type is not support. It is none of: IRI, literal, BNode");
 		return null;
@@ -89,8 +93,9 @@ public class RDFStarTimestampingPlugin extends PluginBase implements StatementLi
 	public void transactionCompleted(PluginConnection pluginConnection) {
 		getLogger().info("transactionCompleted");
 
-			/*if (updateString != null) {
+			if (updateString != null) {
 				getLogger().info("Timestamping previously added triple");
+				repo = new SPARQLRepository(postEndpoint);
 				try (RepositoryConnection connection = repo.getConnection()) {
 					triplesTimestamped = true;
 					repo.init();
@@ -105,7 +110,7 @@ public class RDFStarTimestampingPlugin extends PluginBase implements StatementLi
 					updateString = null;
 					triplesTimestamped = false;
 				}
-			}*/
+			}
 	}
 
 	@Override
