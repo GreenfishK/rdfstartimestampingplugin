2b55b30bf75a88c7d3445bbb81d9d3d793968c6f
Filip Kovacevic <f.kovacevic@gmx.at>
2022-03-10 14:15:52 +0100

    <feat>: Add timestamped insert with data triple deletion. Whenever a triple gets insert, it gets replaced by two nested triples where the data triple is nested inside and annotated with a start and end timestamp.

--- src/main/java/com/ontotext/trree/plugin/rdfstartimestamping/RDFStarTimestampingPlugin.java
+++ src/main/java/com/ontotext/trree/plugin/rdfstartimestamping/RDFStarTimestampingPlugin.java
@@ -11,7 +11,17 @@ import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.MessageFormat;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class RDFStarTimestampingPlugin extends PluginBase implements StatementListener, PluginTransactionListener{
 
@@ -41,19 +51,19 @@ public class RDFStarTimestampingPlugin extends PluginBase implements StatementLi
 	@Override
 	public boolean statementAdded(long subject, long predicate, long object, long context, boolean isAddition, PluginConnection pluginConnection) {
 		Value s = pluginConnection.getEntities().get(subject);
-		Value o = pluginConnection.getEntities().get(predicate);
-		Value p = pluginConnection.getEntities().get(object);
+		Value p = pluginConnection.getEntities().get(predicate);
+		Value o = pluginConnection.getEntities().get(object);
 		Value c = pluginConnection.getEntities().get(context);
 		getLogger().info("Statement added:" + s + " " + p + " " + o + " within context:" + c);
 
 		if (!triplesTimestamped) {
-			updateString = String.format("insert {graph %s {<< %s %s %s >> " +
-							"<http://example.com/metadata/versioning#valid_from> ?timestamp }} " +
-							"where {BIND(<http://www.w3.org/2001/XMLSchema#dateTime>(NOW()) AS ?timestamp) }",
-					entityToString(c),
-					entityToString(s),
-					entityToString(o),
-					entityToString(p));
+			URL res = getClass().getClassLoader().getResource("timestampedInsertTemplate");
+			assert res != null;
+			getLogger().info(res.getPath());
+
+			updateString = MessageFormat.format(readAllBytes("timestampedInsertTemplate"),
+					entityToString(c), entityToString(s), entityToString(p), entityToString(o));
+			getLogger().info(updateString);
 		}
 
 		return false;
@@ -77,6 +87,20 @@ public class RDFStarTimestampingPlugin extends PluginBase implements StatementLi
 			return value.toString();
 		getLogger().error("The entity's type is not support. It is none of: IRI, literal, BNode");
 		return null;
+	}
+
+	private static String readAllBytes(String resourceName) {
+		String text = "";
+		try (InputStream in = RDFStarTimestampingPlugin.class.getResourceAsStream("/" +resourceName)) {
+			assert in != null;
+			text = new BufferedReader(
+					new InputStreamReader(in))
+					.lines()
+					.collect(Collectors.joining("\n"));
+		}  catch (IOException e) {
+			e.printStackTrace();
+		}
+		return text;
 	}
 
 	@Override
--- src/main/java/com/ontotext/trree/plugin/rdfstartimestamping/start.java
+++ src/main/java/com/ontotext/trree/plugin/rdfstartimestamping/start.java
@@ -4,6 +4,13 @@ import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.MessageFormat;
+
 public class start {
 
 
@@ -21,7 +28,22 @@ public class start {
             connection.prepareUpdate(updateString).execute();
             connection.commit();
         }
+        /*URL res = start.class.getClassLoader().getResource("timestampedInsertTemplate");
+        assert res != null;
+        String template= readAllBytes(res);
+        System.out.println(MessageFormat.format(template,"<http://example.com/testGraph>",
+                "<http://example.com/s/v1>","<http://example.com/p/v2>","<http://example.com/o/v3>"));*/
+    }
 
-
+    private static String readAllBytes(URL url)
+    {
+        String content = "";
+        try {
+            content = new String (Files.readAllBytes(Paths.get(url.toURI())));
+        }
+        catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return content;
     }
 }
